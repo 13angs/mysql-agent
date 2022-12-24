8f5b1e71dffe2566d06a162ea3d68a82210c1b3f
13angs <13angz.um@gmail.com>
2022-12-24 16:34:09 +0700

    add backup/restore funtionality

--- run.sh
+++ run.sh
@@ -3,18 +3,18 @@
 source .env
 
 # backup date
-datetime=$(date "+%Y-%m-%d_%H:%M:%S")
+DATETIME=$(date "+%Y-%m-%d_%H:%M:%S")
 
 # backup the db
 backup_db(){
-    echo "Backing up db..."
-    docker exec $CON_NAME mysqldump --all-databases --user=$USERNAME --password=$PASSWORD > "$BACKUP_PATH/all_databases.sql"
+    echo "Backing up db as $DATETIME.sql..."
+    docker exec $CON_NAME mysqldump --all-databases --user=$USERNAME --password=$PASSWORD > "$BACKUP_PATH/$DATETIME.sql"
 }
 
 # restore the db
 restore_db(){
-    echo "Restoring db..."
-    docker exec -i $CON_NAME mysql --user=$USERNAME --password=$PASSWORD < "$BACKUP_PATH/all_databases.sql"
+    echo "Restoring $RESTORE_DB..."
+    docker exec -i $CON_NAME mysql --user=$USERNAME --password=$PASSWORD < "$BACKUP_PATH/$RESTORE_DB"
 }
 
 # restore / backup
