b94294be99dba0007ca0470e6ea93d67fb35a9ec
13angs <13angz.um@gmail.com>
2022-12-24 16:28:52 +0700

    restore using mysql command

--- run.sh
+++ run.sh
@@ -2,5 +2,26 @@
 
 source .env
 
+# backup date
+datetime=$(date "+%Y-%m-%d_%H:%M:%S")
+
 # backup the db
-docker exec $CON_NAME mysqldump --all-databases --user=$USERNAME --password=$PASSWORD > $BACKUP_PATH/all_databases.sql
+backup_db(){
+    echo "Backing up db..."
+    docker exec $CON_NAME mysqldump --all-databases --user=$USERNAME --password=$PASSWORD > "$BACKUP_PATH/all_databases.sql"
+}
+
+# restore the db
+restore_db(){
+    echo "Restoring db..."
+    docker exec -i $CON_NAME mysql --user=$USERNAME --password=$PASSWORD < "$BACKUP_PATH/all_databases.sql"
+}
+
+# restore / backup
+if [ $ACTION == "BACKUP" ]; then
+    backup_db
+elif [ $ACTION == "RESTORE" ]; then
+    restore_db
+else 
+    echo "Incorrect action"
+fi 
